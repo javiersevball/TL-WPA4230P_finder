03525b412a6b66ba95be53c3c7a4a856b9fc4047
javiersevball <Javier.SevBall@gmail.com>
2016-08-10 22:36:24 +0200

    Main algorithm replaced by a simpler one

--- finder.py
+++ finder.py
@@ -21,23 +21,12 @@
 
 import sys
 import socket
-import struct
-import binascii
 import netifaces
 import ipaddress
-import webbrowser
+import subprocess
 
 SOCKET_TIMEOUT = 0.1
-TPLINK_MAC_ADDRESS = 'ec086b'
-# EtherType code for ARP
-ETHERTYPE_ARP = 0x0806
-# ARP packet data
-HTYPE = 0x0001
-PTYPE = 0x0800
-HLEN = 0x0006
-PLEN = 0x0004
-OP_REQUEST = 0x0001
-OP_REPLY = 0x0002
+HTTP_PORT = 80
 
 
 # Calculate the CIDR value for a given mask
@@ -54,120 +43,79 @@ def maskToCIDR(mask):
 
     return cidr
 
-# sender = (IP, MAC) ; target = (IP, MAC) ; type = REQUEST/REPLY
-def createARP(sender, target, type):
-    # Convert IP and MAC addresses into a sequence of bytes
-    senderIP = socket.inet_aton(sender[0])
-    senderMAC = binascii.unhexlify(sender[1])
-    targetIP = socket.inet_aton(target[0])
-    targetMAC = binascii.unhexlify(target[1])
 
-    # Build Ethernet header
-    ethernetHeader = struct.pack('!6s6sh', targetMAC, senderMAC, ETHERTYPE_ARP)
-
-    # Build ARP packet
-    if type == 'REQUEST':
-        arpHeader = struct.pack('!HHBBH', HTYPE, PTYPE, HLEN, PLEN, OP_REQUEST)
-    else:
-        arpHeader = struct.pack('!HHBBH', HTYPE, PTYPE, HLEN, PLEN, OP_REPLY)
-
-    arpPacket = ethernetHeader + arpHeader + senderMAC + senderIP + targetMAC + targetIP
-
-
-    return arpPacket
-
-# Send an ARP REQUEST and wait for response
-def sendARP_request(sock, ipHost, macHost, ipTarget):
-    sender = (ipHost, macHost)
-    target = (ipTarget, 'ffffffffffff')
-    pcktARP_request = createARP(sender, target, 'REQUEST')
-    
-    sock.send(pcktARP_request)
-    pcktARP_reply = sock.recv(1024)
-
-    # Extract IP and MAC addresses of the sender of the ARP REPLY packet
-    # Ethernet header: 14 octets (target + sender + ethertype)
-    # ARP packet: MAC of the sender = octets [8, 14) ; IP of the sender = octets [14, 18).
-    senderReply_MAC = binascii.hexlify(pcktARP_reply[22:28])
-    senderReply_IP = socket.inet_ntoa(pcktARP_reply[28:32])
-
-    return (senderReply_IP, senderReply_MAC.decode('utf-8'))
-
-# Check if the HTTP port of a given IP is open
-def checkHTTPPort(ip):
+# Check if a device is a TL-WPA4230P powerline adapter
+def checkTLWPA4230P(ip):
     cont = True
-    result = True
+    isTLWPA4230P = False
 
     try:
-        sockStream = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     except socket.error:
-        print('An error occurred while creating a socket. Cant check if the HTTP port of {} is open'.format(ip))
+        print('An error ocurred while creating a socket. Cant check if {} is a TL-WPA4230P powerline adapter.'.format(ip))
         cont = False
 
     if cont:
-        error = sockStream.connect_ex((ip, 80))
+        sock.settimeout(SOCKET_TIMEOUT)
+        connectResult = sock.connect_ex((ip, HTTP_PORT))
 
-        if error != 0:
-            result = False
+        if connectResult == 0:
+            httpGET = 'GET / HTTP/1.1\r\nHost: {}\r\n\r\n'.format(ip)
+            sock.send(httpGET.encode('utf-8'))
 
-        sockStream.close()
+            httpRecv = ''
+            transmissionCompleted = False
+            sock.settimeout(None)
+            
+            while not transmissionCompleted:
+                resp = sock.recv(1024)
 
-    return result
+                if resp:
+                    try:
+                        respDec = resp.decode('utf-8')
+                    except UnicodeDecodeError:
+                        respDec = ''
+
+                    httpRecv += respDec
+                else:
+                    transmissionCompleted = True
+
+            if '<title>TL-WPA4230P</title>' in httpRecv:
+                isTLWPA4230P = True
+
+        sock.close()
+    
+    return isTLWPA4230P
+
 
 def main(iface):
-    # Get IP, mask, and MAC addresses
+    # Get IP and mask
     ipHost = netifaces.ifaddresses(iface)[netifaces.AF_INET][0]['addr']
     mask = netifaces.ifaddresses(iface)[netifaces.AF_INET][0]['netmask']
-    macHost = netifaces.ifaddresses(iface)[netifaces.AF_LINK][0]['addr']
-    macHost = macHost.replace(':', '')
 
     # Calculate the CIDR value
     maskCIDR = maskToCIDR(mask)
 
-    # Create the socket and bind it to the given interface
-    cont = True
+    tlwpa4230pIPs = []
 
-    try:
-        sock = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETHERTYPE_ARP))
-    except socket.error:
-        print('An error occurred while creating the RAW socket. Make sure you have sufficient privileges to create it.')
-        cont = False
+    network = ipaddress.ip_network('{}/{}'.format(ipHost, maskCIDR), strict=False)
+    print ('-- Scanning {} addresses...'.format(network.num_addresses - 2))
 
-    if cont:
-        sock.bind((iface, ETHERTYPE_ARP))
-        sock.settimeout(SOCKET_TIMEOUT)
+    for addr in network.hosts():
+        # Check if the addr belongs to a TL-WPA4230P powerline adapter
+        res = checkTLWPA4230P(str(addr))
 
-        tplinkIPs = []
+        if res:
+            print('{} is a TL-WPA4230P powerline adapter!'.format(str(addr)))
+            tlwpa4230pIPs.append(str(addr))
 
-        networkAddr = ipaddress.ip_network('{}/{}'.format(ipHost, maskCIDR), strict=False)
-        print('-- Scanning {} addresses...'.format(networkAddr.num_addresses - 2))
+    print('-- Scan completed.')
 
-        for addr in networkAddr.hosts():
-            waitForMAC = True
-
-            while waitForMAC:
-                try:
-                    sender = sendARP_request(sock, ipHost, macHost, str(addr))
-
-                    if sender[0] == str(addr):
-                        print('Found: {}, {}'.format(sender[0], sender[1]))
-
-                        if TPLINK_MAC_ADDRESS in sender[1] and checkHTTPPort(sender[0]):
-                            print('{} is a TP-LINK product and its HTTP port is open!'.format(sender[0]))
-                            tplinkIPs.append(sender[0])
-
-                        waitForMAC = False
-                
-                except socket.timeout:
-                    waitForMAC = False
-
-        print('-- Scan completed.')
-        sock.close()
-
-        for ip in tplinkIPs:
-            print('Opening {} in web browser...'.format(ip))
-            webbrowser.open('http://{}'.format(ip))
-
+    # Open the web interface of the powerline adapters found
+    for ip in tlwpa4230pIPs:
+        print('Opening {} in web browser...'.format(ip))
+        xdgOpenProcess = subprocess.Popen(['xdg-open', 'http://{}'.format(ip)], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+        xdgOpenProcess.wait()
 
 
 if __name__ == '__main__':
@@ -176,5 +124,5 @@ if __name__ == '__main__':
     else:
         iface = input('Interface?: ')
 
-    main('wlan0')
+    main(iface)
 
